f1c17b805a9264a7436a1008e3e9e8c63637eec2
martinHype <batora.martin33@gmail.com>
2022-10-24 13:06:16 +0200

    added new file where will be displayed the main screen of the app

---
+++ app/src/main/java/com/example/carsharingapp/MapsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.carsharingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps_screen)
+    }
+}
